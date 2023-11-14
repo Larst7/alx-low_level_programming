fa4556f8e75bdd99dd2e3a77a6cf03cbcfb28f78
Larst7 <lasgwenzi7@gmail.com>
2023-11-13 16:11:10 -0800

    libhack.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P
+wget -P .. https://rawgithubusercontent.com/Larst7/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD = "$PWD/ ../libhack.so"
